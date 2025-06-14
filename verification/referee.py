56d38292e09c7a1522a76747272c10bbf4799379
Oleksandr Zozulia <freemanlex@gmail.com>
2025-06-14 22:06:38 +0300

    upd

--- verification/referee.py
+++ verification/referee.py
@@ -11,8 +11,8 @@ api.add_listener(
         tests=TESTS,
         # checker=to_list,
         function_name={
-            "python": "sum_two",
-            "js": "sumTwo"
+            "python": "stones_placing",
+            "js": "stonesPlacing"
         },
         cover_code={
             'python-3': {},
